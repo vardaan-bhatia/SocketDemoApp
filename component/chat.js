3a000d46b2a2bef99e6e58ac48afe13a81826c76
Vardaan Bhatia <vardaanbhatia55@gmail.com>
2024-09-02 07:51:27 +0530

    cors error fixed

--- component/chat.js
+++ component/chat.js
@@ -41,20 +41,33 @@ export default function Chat() {
   };
 
   return (
-    <div>
-      <div>
-        {messages.map((msg, index) => (
-          <p key={index}>{msg}</p>
-        ))}
+    <div className="flex flex-col h-full max-w-lg mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+      <div className="flex-1 p-4 overflow-y-auto bg-gray-100">
+        <div className="space-y-2">
+          <h1 className="font-bold text-2xl">Messages</h1>
+          {messages.map((msg, index) => (
+            <div key={index} className="p-2 bg-blue-100 rounded-lg">
+              <p>{msg}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyPress={handleKey} // Attach the event handler
-        placeholder="Type a message..."
-      />
-      <button onClick={sendMessage}>Send</button>
+      <div className="flex items-center border-t border-gray-200 bg-white p-2 ml-12">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={handleKey}
+          className="flex-1 p-2 border border-gray-300 rounded-l-lg"
+          placeholder="Type a message..."
+        />
+        <button
+          onClick={sendMessage}
+          className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-r-lg"
+        >
+          Send
+        </button>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,15 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      <h1>Real-Time Chat with Socket.IO</h1>
-      <Chat />
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-4 bg-white rounded shadow-md">
+        <h1 className="text-2xl font-bold mb-4">
+          Real-Time Chat with Socket.IO
+        </h1>
+        <div className="flex items-center justify-center">
+          <Chat />
+        </div>
+      </div>
     </div>
   );
 }
